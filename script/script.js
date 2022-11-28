bd10ae601f21c594f3bc5cafb6afe74a88792305
Vo Ta Thanh An <votathanhan456@gmail.com>
2022-11-28 10:38:56 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function onclickPopUp() {
+    document.getElementById("chatbox").style.display = "block";
+}
+function onclickChatTop() {
+    document.getElementById("chatbox").style.display = "none";
+}
